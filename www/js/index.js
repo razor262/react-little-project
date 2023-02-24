e58ee78c5ca5edc4496e50faf045660173a62d8e
Seven <34629679+razor262@users.noreply.github.com>
2023-02-24 22:31:05 +0300

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+// ReactDOM.render(React.createElement('input', {
+//     placeholder: "Help text",
+//     onClick: () => console.log("Clicked"),
+//     onMouseEnter: () => console.log("Mouse Over"),
+// }), document.getElementById("app"))
+
+
+const elements = <input placeholder="Help text" onClick={} />
+const app = document.getElementById("app")
+
+ReactDOM.render(elements, app)
